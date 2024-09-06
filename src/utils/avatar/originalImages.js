b3f238183157eda53c1acd1c8402cedb68bec06b
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-09-06 08:23:35 +0530

    update templates import issue

--- src/utils/avatar/originalImages.js
+++ src/utils/avatar/originalImages.js
@@ -1,52 +1,29 @@
 import {
-  maleOriginal01, 
-  maleOriginal02 , 
-  maleOriginal03 , 
-  maleOriginal04 , 
-  maleOriginal05 , 
-  maleOriginal06 , 
-  maleOriginal07 , 
-  maleOriginal08 , 
-  maleOriginal09, 
-  maleOriginal10,
+  maleOriginal01,
+  maleOriginal02,
+  maleOriginal03,
+  maleOriginal04,
 } from "../../assets/avatar/Originals/male-originals";
 
-import {   
-  femaleOriginal01, 
-  femaleOriginal02 , 
-  femaleOriginal03 , 
-  femaleOriginal04 , 
-  femaleOriginal05 , 
-  femaleOriginal06 , 
-  femaleOriginal07 , 
-  femaleOriginal08 , 
-  femaleOriginal09, 
-  femaleOriginal10, } from '../../assets/avatar/Originals/female-originals'
+import {
+  femaleOriginal01,
+  femaleOriginal02,
+  femaleOriginal03,
+  femaleOriginal04,
+} from "../../assets/avatar/Originals/female-originals";
 
- const maleOriginalArr = [
-  maleOriginal01, 
-  maleOriginal02 , 
-  maleOriginal03 , 
-  maleOriginal04 , 
-  maleOriginal05 , 
-  maleOriginal06 , 
-  maleOriginal07 , 
-  maleOriginal08 , 
-  maleOriginal09, 
-  maleOriginal10,
+const maleOriginalArr = [
+  maleOriginal01,
+  maleOriginal02,
+  maleOriginal03,
+  maleOriginal04,
 ];
 
 const femaleOriginalArr = [
-  femaleOriginal01, 
-  femaleOriginal02 , 
-  femaleOriginal03 , 
-  femaleOriginal04 , 
-  femaleOriginal05 , 
-  femaleOriginal06 , 
-  femaleOriginal07 , 
-  femaleOriginal08 , 
-  femaleOriginal09, 
-  femaleOriginal10,
-]
+  femaleOriginal01,
+  femaleOriginal02,
+  femaleOriginal03,
+  femaleOriginal04,
+];
 
-export { maleOriginalArr,femaleOriginalArr}
+export { maleOriginalArr, femaleOriginalArr };
