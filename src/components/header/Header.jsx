f3c332606b72653731b17448ce386d2438b43357
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-09-06 15:06:53 +0530

    build error solved

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./header.module.css";
 import { Link } from "react-router-dom";
-import logo from "./../../assets/header/logo.png";
+import logo from "./../../assets/logo.png";
 
 export default function Header() {
   return (
